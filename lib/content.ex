95d9eab6e8b6c3288a9a7063355d16d7d1da63bb
kwardynski <kacper.wardynski@gmail.com>
2024-11-10 16:07:56 -0700

    fix sorting - do by date key

--- lib/content.ex
+++ lib/content.ex
@@ -7,7 +7,7 @@ defmodule Blog.Content do
     as: :posts,
     highlighters: [:makeup_elixir, :makeup_erlang]
 
-  @posts Enum.sort_by(@posts, & &1, {:desc, Date})
+  @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
 
   def all_posts, do: @posts
 end
